560dfde99424c6fc0951df1b3bedef9ba9aed411
Ana Silveira <ana.ccord@gmail.com>
2023-03-13 11:19:31 -0300

    chore: Aula 11 - Princípios de Clean Code e programação declarativa

--- script.js
+++ script.js
@@ -16,34 +16,38 @@ const secondsDisplay = document.querySelector('.seconds')
 //programação imperativa
 //callback
 
+function resetControls() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')    
+    buttonStop.classList.add('hide')
+
+}
+
+function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
 function countdown(){
     setTimeout(function() {
         let seconds = Number(secondsDisplay.textContent)
         let minutes = Number(minutesDisplay.textContent)
 
-        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+        updateTimerDisplay(minutes, 0)
 
         if (minutes <= 0) {
-
-            buttonPlay.classList.remove('hide')
-            buttonPause.classList.add('hide')
-            buttonSet.classList.remove('hide')    
-            buttonStop.classList.add('hide')
-
-            
+            resetControls()            
             return
         }
         
 
         if(seconds <= 0 ) {
             seconds = 2
-
-            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+            --minutes
         }
 
-        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-
-
+        updateTimerDisplay(minutes, String(seconds - 1))
 
         countdown()
     }, 1000)
@@ -64,11 +68,8 @@ buttonPause.addEventListener('click', function() {
 
 })
 
-buttonStop.addEventListener('click', function() {    
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')    
-    buttonStop.classList.add('hide')
+buttonStop.addEventListener('click', function() {   
+    resetControls()
 })
 
 buttonSoundOn.addEventListener('click', function() {    
@@ -83,5 +84,5 @@ buttonSoundOff.addEventListener('click', function() {
 
 buttonSet.addEventListener('click', function() {
     minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    updateTimerDisplay(minutes, 0)
 })
