586ff7e308c39bb0085b9f3e7e14df7a78345668
Ana Silveira <ana.ccord@gmail.com>
2023-03-13 18:07:17 -0300

    chore: Aula 13 - Introdução ao ES6 Modules

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import resetControls from "./timer.js"
+import "./controls.js"
 // DOM
 // Document Object Model
 
@@ -17,47 +19,6 @@ let timerTimeOut
 //programação imperativa
 //callback
 
-function resetControls() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')    
-    buttonStop.classList.add('hide')
-
-}
-
-function resetTimer() {
-    updateTimerDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
-}
-
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function countdown(){
-    timerTimeOut = setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        updateTimerDisplay(minutes, 0)
-
-        if (minutes <= 0) {
-            resetControls()            
-            return
-        }
-        
-
-        if(seconds <= 0 ) {
-            seconds = 2
-            --minutes
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1))
-
-        countdown()
-    }, 1000)
-}
 
 buttonPlay.addEventListener('click', function() {    
     buttonPlay.classList.add('hide')
